ac4a200d41e48c478273007a7a82126eeab97e26
shon ben shimon <shonbenshimon@Shons-MacBook-Pro.local>
2022-03-22 10:32:36 +0200

    corrected spaces

--- Week2/Day2/daily_challenge/script.js
+++ Week2/Day2/daily_challenge/script.js
@@ -1,7 +1,7 @@
 let sentence = "The movie is not that bad I like it."
-let sentenceArr = sentence.split(" ")
-var wordNot = sentenceArr.indexOf("not")
-var wordBad = sentenceArr.indexOf("bad")
+let sentenceSplit = sentence.split(" ")
+var wordNot = sentenceSplit.indexOf("not")
+var wordBad = sentenceSplit.indexOf("bad")
 
 if ( wordBad > wordNot ) {
 	console.log(sentence.replace("not that bad","good,"));
