cfe762ebe704c536609487319d0b4d057872dc5b
shon ben shimon <shonbenshimon@Shons-MacBook-Pro.local>
2022-03-29 17:36:43 +0300

    finished exersice xpweek 3 day 3

---
+++ Week3/Day3/exercise_xp/script.js
@@ -0,0 +1,10 @@
+let container = document.body.children[1]
+console.log(container)
+
+let square = document.getElementById("animate")
+console.log(square)
+
+function myMove(){
+	square.style.left = "350px"
+}
+
