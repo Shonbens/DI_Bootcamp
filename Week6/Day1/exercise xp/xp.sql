179dd5be357db68b9ed870ebce977f4632b6afd6
shon ben shimon <shonbenshimon@Shons-MacBook-Pro.local>
2022-04-25 09:44:17 +0300

    finished exercise xp week 5 day 1

---
+++ Week6/Day1/exercise xp/xp.sql
@@ -0,0 +1,40 @@
+/*
+CREATE TABLE items (
+  id INTEGER PRIMARY KEY,
+  item varchar,
+  price Float
+);
+
+CREATE TABLE costumers (
+  id INTEGER PRIMARY KEY,
+  person varchar,
+  last_name varchar
+);
+*/
+
+/*
+insert into items (id,item, price) values
+(1, 'Small Desk', 100),
+(2, 'Large Desk', 300),
+(3, 'Fan', 80);
+*/
+
+/*
+insert into costumers (id,person,last_name) values
+(1, 'Greg', 'Jones'),
+(2, 'Sandra', 'Jones'),
+(3, 'Scott', 'Scott'),
+(4, 'Trevor', 'Green'),
+(5, 'Melanie', 'Johnson');
+*/
+
+/*
+select * from items
+select item from items where price >80
+select item from items where price <= 300
+select last_name from costumers where last_name = 'Smith'
+select last_name from costumers where last_name = 'Jones'
+select last_name from costumers where person != 'Scott'
+*/
+
+
